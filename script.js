ccf29c87a0ce8f56ad3d643e7b82217cd20414a5
lazerface175 <124861983+lazerface175@users.noreply.github.com>
2023-03-04 02:51:32 -0800

    added win or lose function

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-const array = [
+const choices = [
     "rock",
     "paper",
     "scissors"
 ];
 
-function getComputerChoice(array){
-    const randomIndex = Math.floor(Math.random()*array.length);
-    const item = array[randomIndex];
+let promtChoice = "scissors"
+let playerChoice = promtChoice.toLocaleLowerCase();
+
+function getComputerChoice(){ //I dont know what the (array) paramater is used for
+    const randomIndex = Math.floor(Math.random()*choices.length);
+    const item = choices[randomIndex];
     return item;
 }
 
-const result = getComputerChoice(array);
-console.log(result)
+function singleRound(playerSeletion, computerSelection){
+    if (playerSeletion === computerSelection){
+        return "TIE!";
+    } else if (playerSeletion === "rock" && computerSelection === "scissors"){
+        return "You win!"
+    } else if (playerSeletion === "rock" && computerSelection === "paper"){
+        return "You lose!"
+    } else if (playerSeletion === "paper" && computerSelection === "rock"){
+        return "You win!"
+    } else if (playerSeletion === "paper" && computerSelection === "scissors"){
+        return "You lose!"
+    } else if (playerSeletion === "scissors" && computerSelection === "paper"){
+        return "You win!"
+    } else if (playerSeletion === "scissors" && computerSelection === "rock"){
+        return "You lose!"
+    }
+}
+
+const computerResult = getComputerChoice(); //I dont know what the (array) argument is used for
+console.log(computerResult)
+console.log(singleRound(playerChoice, computerResult))
