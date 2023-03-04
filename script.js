a4e2336e30afd64048b45a17819a58d277a62a14
lazerface175 <124861983+lazerface175@users.noreply.github.com>
2023-03-04 05:41:17 -0800

    Finished game

--- script.js
+++ script.js
@@ -4,45 +4,73 @@ const choices = [
     "scissors"
 ];
 
-let promtChoice = prompt("pick rock, paper, or scissors");
-let playerChoice = promtChoice.toLocaleLowerCase();
+
 let playerScore = 0
 let computerScore = 0
 
-function getComputerChoice(){ //I dont know what the (array) paramater is used for
-    const randomIndex = Math.floor(Math.random()*choices.length);
-    const item = choices[randomIndex];
-    return item;
-}
+for(i = 0; i < 5; i ++){
 
-function singleRound(playerSeletion, computerSelection){
-    if (playerSeletion === computerSelection){
-        gameResult = "TIE!";
-    } else if (playerSeletion === "rock" && computerSelection === "scissors"){
-        gameResult = "You win!"
-        playerScore = playerScore + 1;
-    } else if (playerSeletion === "rock" && computerSelection === "paper"){
-        gameResult = "You lose!"
-        computerScore = computerScore + 1;
-    } else if (playerSeletion === "paper" && computerSelection === "rock"){
-        gameResult = "You win!"
-        playerScore = playerScore + 1;
-    } else if (playerSeletion === "paper" && computerSelection === "scissors"){
-        gameResult = "You lose!"
-        computerScore = computerScore + 1;
-    } else if (playerSeletion === "scissors" && computerSelection === "paper"){
-        gameResult = "You win!"
-        playerScore = playerScore + 1;
-    } else if (playerSeletion === "scissors" && computerSelection === "rock"){
-        gameResult = "You lose!"
-        computerScore = computerScore + 1
+    let promtChoice = prompt("pick rock, paper, or scissors");
+    let playerChoice = promtChoice.toLocaleLowerCase();
+    let computerResult = getComputerChoice();
+
+    singleRound(playerChoice, computerResult);
+
+    function getComputerChoice(){ //I dont know what the (array) paramater is used for
+        const randomIndex = Math.floor(Math.random()*choices.length);
+        const item = choices[randomIndex];
+        return item;
+    }
+
+    function singleRound(playerSeletion, computerSelection){
+
+        if (playerSeletion === computerSelection){
+            gameResult = "TIE!";
+            alert(`I picked ${computerSelection}\nIt's a ${gameResult}\nYour score is: ${playerScore}\nMy score is: ${computerScore}`);
+        } else if (playerSeletion === "rock" && computerSelection === "scissors"){
+            gameResult = "You win!"
+            playerScore = playerScore + 1;
+            alert(`I picked ${computerSelection}\n${gameResult}\nYour score is: ${playerScore}\nMy score is: ${computerScore}`);
+        } else if (playerSeletion === "rock" && computerSelection === "paper"){
+            gameResult = "You lose!"
+            computerScore = computerScore + 1;
+            alert(`I picked ${computerSelection}\n${gameResult}\nYour score is: ${playerScore}\nMy score is: ${computerScore}`);
+        } else if (playerSeletion === "paper" && computerSelection === "rock"){
+            gameResult = "You win!"
+            playerScore = playerScore + 1;
+            alert(`I picked ${computerSelection}\n${gameResult}\nYour score is: ${playerScore}\nMy score is: ${computerScore}`);
+        } else if (playerSeletion === "paper" && computerSelection === "scissors"){
+            gameResult = "You lose!"
+            computerScore = computerScore + 1;
+            alert(`I picked ${computerSelection}\n${gameResult}\nYour score is: ${playerScore}\nMy score is: ${computerScore}`);
+        } else if (playerSeletion === "scissors" && computerSelection === "paper"){
+            gameResult = "You win!"
+            playerScore = playerScore + 1;
+            alert(`I picked ${computerSelection}\n${gameResult}\nYour score is: ${playerScore}\nMy score is: ${computerScore}`);
+        } else if (playerSeletion === "scissors" && computerSelection === "rock"){
+            gameResult = "You lose!"
+            computerScore = computerScore + 1
+            alert(`I picked ${computerSelection}\n${gameResult}\nYour score is: ${playerScore}\nMy score is: ${computerScore}`);
+        }
+    }
+
+
+    //const computerResult = getComputerChoice(); //I dont know what the (array) argument is used for
+    console.log(computerResult)
+    //singleRound(playerChoice, computerResult)
+    console.log(gameResult)
+    console.log("player: " + playerScore)
+    console.log("computer: " + computerScore)
+    //console.log(singleRound(playerChoice, computerResult))
+};
+
+function endGame(){
+    if (playerScore === computerScore){
+        alert(`Your score was: ${playerScore}\nAnd my score was: ${computerScore}\nIt's a TIE!\nRefresh the page to play again!`);
+    } else if(playerScore > computerScore){
+        alert(`Your score was: ${playerScore}\nAnd my score was: ${computerScore}\nYou WIN!\nRefresh the page to play again!`);
+    } else if(playerScore < computerScore){
+        alert(`Your score was: ${playerScore}\nAnd my score was: ${computerScore}\nYou LOST\nRefresh the page to play again!`);
     }
 }
-
-const computerResult = getComputerChoice(); //I dont know what the (array) argument is used for
-console.log(computerResult)
-singleRound(playerChoice, computerResult)
-console.log(gameResult)
-console.log("player: " + playerScore)
-console.log("computer: " + computerScore)
-//console.log(singleRound(playerChoice, computerResult))
+endGame();
