445b0fdd0696145201505806da2a4b81f19b30ea
lazerface175 <124861983+lazerface175@users.noreply.github.com>
2023-03-04 04:01:35 -0800

    added a score return

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const choices = [
     "scissors"
 ];
 
-let promtChoice = "scissors"
+let promtChoice = prompt("pick rock, paper, or scissors");
 let playerChoice = promtChoice.toLocaleLowerCase();
+let playerScore = 0
+let computerScore = 0
 
 function getComputerChoice(){ //I dont know what the (array) paramater is used for
     const randomIndex = Math.floor(Math.random()*choices.length);
@@ -15,22 +17,32 @@ function getComputerChoice(){ //I dont know what the (array) paramater is used f
 
 function singleRound(playerSeletion, computerSelection){
     if (playerSeletion === computerSelection){
-        return "TIE!";
+        gameResult = "TIE!";
     } else if (playerSeletion === "rock" && computerSelection === "scissors"){
-        return "You win!"
+        gameResult = "You win!"
+        playerScore = playerScore + 1;
     } else if (playerSeletion === "rock" && computerSelection === "paper"){
-        return "You lose!"
+        gameResult = "You lose!"
+        computerScore = computerScore + 1;
     } else if (playerSeletion === "paper" && computerSelection === "rock"){
-        return "You win!"
+        gameResult = "You win!"
+        playerScore = playerScore + 1;
     } else if (playerSeletion === "paper" && computerSelection === "scissors"){
-        return "You lose!"
+        gameResult = "You lose!"
+        computerScore = computerScore + 1;
     } else if (playerSeletion === "scissors" && computerSelection === "paper"){
-        return "You win!"
+        gameResult = "You win!"
+        playerScore = playerScore + 1;
     } else if (playerSeletion === "scissors" && computerSelection === "rock"){
-        return "You lose!"
+        gameResult = "You lose!"
+        computerScore = computerScore + 1
     }
 }
 
 const computerResult = getComputerChoice(); //I dont know what the (array) argument is used for
 console.log(computerResult)
-console.log(singleRound(playerChoice, computerResult))
+singleRound(playerChoice, computerResult)
+console.log(gameResult)
+console.log("player: " + playerScore)
+console.log("computer: " + computerScore)
+//console.log(singleRound(playerChoice, computerResult))
